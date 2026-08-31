c6fd6b7c61773385512747d24f330ddf9b434d67
firebadnofire <william.jones@archuser.org>
2026-08-31 15:35:24 -0400

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "v0.1.0"
+        versionName = "v0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
